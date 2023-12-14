79ac34266f3262969ef0c08ddcf48bbe8f7a2bad
alfredangeles <alf.angeles@gmail.com>
2023-12-14 09:53:48 -0500

    Set computerSelection to return Rock Paper or Scissors.

--- script.js
+++ script.js
@@ -3,9 +3,23 @@
 
 function getComputerChoice() {
     let ranNUm = Math.random();
-    let computerChoice = Math.floor(ranNUm * 3)+1;
-    console.log(ranNUm);
-    console.log(computerChoice);
+    let computerChoiceNumeric = Math.floor(ranNUm * 3)+1; // random number betwen 1 an 3
+
+    let computerChoice
+
+    switch (computerChoiceNumeric) {
+        case 1:
+            computerChoice = "Rock";
+            break;
+        case 2:
+            computerChoice = "Paper";
+            break;
+        case 3:
+            computerChoice = "Scissors";
+            break;
+    }
+
+    return computerChoice
 }
 
 console.log(getComputerChoice());
